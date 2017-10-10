28ae5e4178d3cd888b22ae6f01c0e9961366b9c9
volkekevo <kelvinquria@gmail.com>
2017-10-10 18:17:15 +0300

    Initial commit

---
+++ app/src/main/java/ke/co/impsoft/impsoftsolution/MainActivity.java
@@ -0,0 +1,35 @@
+package ke.co.impsoft.impsoftsolution;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mywebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebview = (WebView)findViewById(R.id.webview);
+
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebview.loadUrl("http://impsoft.co.ke/");
+        //enable java script
+        mywebview.setWebViewClient(new WebViewClient());
+        //block external browser
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mywebview.canGoBack()){
+            mywebview.goBack();
+        }else {
+        super.onBackPressed();
+    }
+}}
